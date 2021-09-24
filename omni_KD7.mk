fced9fee2d6b5e570aad219c87d0abe2c9c9883f
mastersenpai05 <pampangavergil0405@gmail.com>
2021-09-24 16:56:46 +0800

    KD7: add fastbootd

--- omni_KD7.mk
+++ omni_KD7.mk
@@ -39,3 +39,8 @@ PRODUCT_TARGET_VNDK_VERSION := 29
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
