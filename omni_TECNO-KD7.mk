0db1a2b152f5e75d64b7f469bf1c15c06aeb10e0
mastersenpai05 <pampangavergil0405@gmail.com>
2021-09-24 15:38:45 +0800

    kd7: clean up

--- omni_TECNO-KD7.mk
+++ omni_TECNO-KD7.mk
@@ -18,16 +18,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from KD7 device
-$(call inherit-product, device/tecno/KD7/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := TECNO-KD7
